fc943cc925079a6c06ff9c8679fe9f40d6f82b0a
Dianajarenga <79199013+Dianajarenga@users.noreply.github.com>
2021-04-20 08:48:13 +0300

    Add files via upload

---
+++ main/BLUE.ino
@@ -0,0 +1,8 @@
+void BLUE (){
+digitalWrite(Blue,HIGH);
+delay(1000);
+digitalWrite(Blue,LOW);
+delay(1000);
+  
+  
+  }
---
+++ main/GREEN.ino
@@ -0,0 +1,5 @@
+void GREEN(){
+digitalWrite(Green,HIGH);
+delay(1000);
+digitalWrite(Green,LOW);
+delay(1000); }
